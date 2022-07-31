b183734fc1494f0645737ea649b7a3ae5ff5df4a
JunSeo <oponize@naver.com>
2022-07-31 13:40:18 +0900

    feat: lib 추가

--- src/components/auth/authLayout.tsx
+++ src/components/auth/authLayout.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import style from "./style.module.css";
 
 const AuthView = ({ children }: { children: React.ReactNode }) => {
-  return <main className={`${style.container}`}>{children}</main>;
+  return <main>{children}</main>;
 };
 
 export default AuthView;
